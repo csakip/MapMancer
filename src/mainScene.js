edbf3e31c3d1c3437e4ce061da0fb5dbe2d3c70f
Peter Csaki <csakip01@gmail.com>
2023-04-10 23:11:39 +0100

    Camera fixes and canvas size.

--- src/mainScene.js
+++ src/mainScene.js
@@ -1,9 +1,7 @@
 import Phaser from "phaser";
 
 export class MainScene extends Phaser.Scene {
-  zoomTo = 1;
-
-  constructor() {
+  constructor(game) {
     super();
   }
 
@@ -14,11 +12,9 @@ export class MainScene extends Phaser.Scene {
   create() {
     const camera = this.cameras.main;
     const forest_glade_map = this.textures.get("forest_glade_map");
-    console.log(forest_glade_map.source[0].width);
     this.add.image(0, 0, "forest_glade_map").setOrigin(0, 0);
-    camera.setBounds(0, 0, forest_glade_map.source[0].width, forest_glade_map.source[0].height);
-    camera.setZoom(0.5);
-    this.zoomTo = camera.zoom;
+    camera.centerOn(forest_glade_map.source[0].width / 2, forest_glade_map.source[0].height / 2);
+    camera.setZoom(1);
 
     // Drag map with mouse
     this.input.on("pointermove", (p) => {
@@ -29,29 +25,58 @@ export class MainScene extends Phaser.Scene {
     });
 
     // Zoom on mouse wheel.
-    this.input.on("wheel", (pointer, gameObjects, deltaX, deltaY, deltaZ) => {
-      const zoomStep = 0.2;
+    this.input.on("wheel", (p) => {
+      const zoomStep = 0.3;
+      const game = this.sys.game;
+      let zoomTo = camera.zoom;
 
-      if (deltaY > 0) {
-        this.zoomTo *= 1 - zoomStep;
-      } else if (deltaY < 0) {
-        this.zoomTo *= 1 + zoomStep;
+      if (p.deltaY > 0) {
+        zoomTo *= 1 - zoomStep;
+      } else if (p.deltaY < 0) {
+        zoomTo *= 1 + zoomStep;
+      }
+      let hitZoomExtremes = false;
+      if (zoomTo < 0.05) {
+        zoomTo = 0.05;
+        hitZoomExtremes = true;
+      }
+      if (zoomTo > 2.5) {
+        zoomTo = 2.5;
+        hitZoomExtremes = true;
+      }
+      zoomTo = Math.min(Math.max(zoomTo, 0.05), 2.5);
+
+      // Zoom towards mouse pointer
+      const cameraCenterInWorldX = camera.worldView.x + camera.worldView.width / 2;
+      const cameraCenterInWorldY = camera.worldView.y + camera.worldView.height / 2;
+      const mouseRelativeFromCameraX = game.input.mousePointer.worldX - cameraCenterInWorldX;
+      const mouseRelativeFromCameraY = game.input.mousePointer.worldY - cameraCenterInWorldY;
+
+      let magicNumber = 0.23;
+      if (p.deltaY > 0) {
+        magicNumber = 0.435;
       }
 
-      // TODO: zoom to mouse pointer
+      const xAdjust = mouseRelativeFromCameraX * magicNumber * -Math.sign(p.deltaY);
+      const yAdjust = mouseRelativeFromCameraY * magicNumber * -Math.sign(p.deltaY);
 
-      this.zoomTo = Math.min(Math.max(this.zoomTo, 0.1), 2);
+      // const tweenConfig = {
+      //   targets: camera,
+      //   zoom: zoomTo,
+      //   ease: "Linear",
+      //   duration: 100,
+      //   repeat: 0,
+      //   yoyo: false,
+      //   scrollX: camera.scrollX + xAdjust,
+      //   scrollY: camera.scrollY + yAdjust,
+      // };
 
-      const tweenConfig = {
-        targets: camera,
-        zoom: this.zoomTo,
-        ease: "Linear",
-        duration: 100,
-        repeat: 0,
-        yoyo: false,
-      };
-
-      this.tweens.add(tweenConfig);
+      // this.tweens.add(tweenConfig);
+      camera.zoom = zoomTo;
+      if (!hitZoomExtremes) {
+        camera.scrollX += xAdjust;
+        camera.scrollY += yAdjust;
+      }
     });
   }
 }
